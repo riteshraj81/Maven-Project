8cc3c9c6cc9d5f60ba0cdee17c4bebfb8e1bf05e
riteshraj81 <88712525+riteshraj81@users.noreply.github.com>
2022-01-07 10:55:58 +0530

    Update mydsl.groovy

--- mydsl.groovy
+++ mydsl.groovy
@@ -2,7 +2,7 @@ job('my-dsl-job-1') {
     logRotator(5, 10)
     jdk('Java 8')
     scm {
-        git('https://github.com/admingagan/Maven-Project.git', 'master')
+        git('https://github.com/riteshraj81/Maven-Project.git', 'master')
     }
     triggers {
         githubPush()
